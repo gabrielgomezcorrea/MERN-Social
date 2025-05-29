9b286af2b452fdfaa8a6d74d8ba0269f4424c39c
Gabriel Gomez <gabrielgomezcorrea09@gmail.com>
2025-05-29 15:15:55 -0400

    added post routes and models

---
+++ backend/src/models/Post.js
@@ -0,0 +1,34 @@
+import { model, Schema } from "mongoose";
+
+const PostSchema = new Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    location: String,
+    description: String,
+    picturePath: String,
+    userPicturePath: String,
+    likes: {
+      type: Map,
+      of: Boolean,
+    },
+    comments: {
+      type: Array,
+      default: [],
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = model("Post", PostSchema);
+export default Post;
